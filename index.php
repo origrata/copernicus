a4fd11c3835e6b7858a5374ba2b64eaf3514e364
Ori Grata <42106367+origrata@users.noreply.github.com>
2026-04-22 00:16:22 +0700

    Update copyright information in index.php
    Removed outdated copyright information for origrata.

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
  
 /**
  * @file plugins/importexport/sample/index.php
- *
+ * Copyright (c) 2024-2026 origrata origrata@ioscloud.co.id
  * Copyright (c) 2013-2015 Simon Fraser University Library
  * Copyright (c) 2003-2015 John Willinsky
  * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
